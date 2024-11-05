e24c85e4c3ded21978cec645f1c83ab7a5bd622f
JoYoneyama <jvsyoneyama@gmail.com>
2024-11-05 14:57:21 -0300

    animacoes home e animacao numeros

--- js/funcao.js
+++ js/funcao.js
@@ -49,18 +49,46 @@ window.onresize = function (event) {
 let valueDisplays = document.querySelectorAll(".num");
 let interval = 5000;
 
-valueDisplays.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-    let duration = Math.floor(interval / endValue);
-    let counter = setInterval(function () {
-        startValue += 1;
-        valueDisplay.textContent = startValue;
-        if (startValue == endValue) {
-            clearInterval(counter);
-        }
-    }, duration);
+document.addEventListener("DOMContentLoaded", () => {        
+
+    // Seleciona todos os elementos com a classe 'value-display' para a contagem
+    const valueDisplays = document.querySelectorAll(".value-display");
+    const interval = 1500; // Define um intervalo total para a contagem (em milissegundos)
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                valueDisplays.forEach((valueDisplay) => {
+                    let startValue = 0;
+                    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+                    
+                    // Verifica se endValue é um número válido
+                    if (isNaN(endValue) || endValue <= 0) {
+                        console.error("Atributo data-val inválido:", endValue);
+                        return;
+                    }
+
+                    let duration = Math.floor(interval / endValue);
+                    
+                    let counter = setInterval(function () {
+                        startValue += 1;
+                        valueDisplay.textContent = startValue;
+                        if (startValue === endValue) {
+                            clearInterval(counter);
+                        }
+                    }, duration);
+                });
+
+                // Para observar o elemento apenas uma vez e não reiniciar a contagem
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    // Observa cada elemento de contagem para ativar a animação ao aparecer na tela
+    valueDisplays.forEach((element) => observer.observe(element));
 });
+
 
 
 // codigo para animar os elementos quando aparecem na tela
