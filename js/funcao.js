b42c5ebe6c6e51a2bbb72a81331967d0d348669e
JoYoneyama <jvsyoneyama@gmail.com>
2024-11-04 21:59:54 -0300

    adicionando primeiras animacoes

--- js/funcao.js
+++ js/funcao.js
@@ -61,3 +61,26 @@ valueDisplays.forEach((valueDisplay) => {
         }
     }, duration);
 });
+
+
+// codigo para animar os elementos quando aparecem na tela
+
+//animacao so acontece quando o elemento entra no viewport da tela
+
+document.addEventListener("DOMContentLoaded", () => {         //checa se o documento carregou
+
+    const observer = new IntersectionObserver(entries => {       //usa IntersectionObserver para ver se o elemento esta no viewport
+      entries.forEach(entry => {
+        if(entry.isIntersecting){
+          entry.target.classList.add('in-view');
+          return;
+        }
+        
+      })
+    })
+  
+    const allAnimatedElements = document.querySelectorAll('.animate');       //pega todos os elementos com .animate aplicados
+  
+    allAnimatedElements.forEach((element) => observer.observe(element));      //adiciona o observer para todos esses elementos
+  
+  })
