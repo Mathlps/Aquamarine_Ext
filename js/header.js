2bdd488b3a0a40ab84e94a9ccafa60cace47051e
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-11-10 12:48:24 -0300

    voltando commit perdido

--- js/header.js
+++ js/header.js
@@ -3,3 +3,9 @@ window.addEventListener("scroll", function () {
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+
+const hambuguer = document.querySelector(".hambuguer")
+const nav = document.querySelector(".nav")
+
+hambuguer.addEventListener("click", () => nav.classList.toggle("active"))
