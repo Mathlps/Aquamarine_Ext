619a64d0fb19b5747124ce2a7d4bccab714641f3
Gatasso <galassoworkstation@gmail.com>
2024-11-11 17:18:47 -0300

    feat: adiciona endpoints básicos do backend

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,14 @@
 from flask import Flask
 
 app =  Flask(__name__)
+
+@app.route("/projeto/<int:id>", methods= ['GET'])
+def get_project_by_id(id):
+        return "<h1>PROJETO TAL</h1>"
+
+@app.route("/noticia/<int:id>", methods= ['GET'])
+def get_news_by_id(id):
+    return "<h1>NOTICIA TAL</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
