934e9b02c47d2d0a4058651ae4b179ab53e31585
Gatasso <galassoworkstation@gmail.com>
2024-11-13 17:18:26 -0300

    feat: Adiciona Perfis de desenvolvimento,SQLAlchemy, e arquivo requirements.txt para rodar dependências

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,17 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from db_config import Dev,Prod
 
+db = SQLAlchemy()
 app =  Flask(__name__)
+
+if os.environ.get('FLASK_ENV') == 'production':
+    app.config.from_object(Prod)
+else:
+    app.config.from_object(Dev)
+
+# db = SQLAlchemy(app)
 
 @app.route("/projeto/<int:id>", methods= ['GET'])
 def get_project_by_id(id):
@@ -11,4 +22,4 @@ def get_news_by_id(id):
     return "<h1>NOTICIA TAL</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=app.config['DEBUG'])
---
+++ backend/db_config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave_secreta_padrao'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Dev(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql123@localhost/aquamarine'
+    DEBUG = True
+
+class Prod(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # configurar na hospedagem com a variável deles
+    DEBUG = False
