d73002a3584a6cc4d4013a576db5837cc90ceeb6
Paola Abrantes <167098279+paolaabrantes@users.noreply.github.com>
2024-11-27 00:05:13 -0300

    Atualização de Links e JS para validação de e-mail

--- cadastro.js
+++ cadastro.js
@@ -1,24 +1,20 @@
-// Conta permitida para acesso
 const contaPermitida = {
-    email: "admin@aquamarine.com",
+    email: "admin@aquamarine.com.br",
     senha: "12345"
 };
 
-// Selecionar o formulário e os campos
 const loginForm = document.getElementById("loginForm");
 const emailInput = document.getElementById("email");
 const senhaInput = document.getElementById("senha");
 
 loginForm.addEventListener("submit", (event) => {
-    event.preventDefault(); // Evita o envio do formulário
+    event.preventDefault();
 
     const email = emailInput.value;
     const senha = senhaInput.value;
 
-    // Validação da conta
     if (email === contaPermitida.email && senha === contaPermitida.senha) {
         alert("Login realizado com sucesso!");
-        // Redirecionar para a página principal
         window.location.href = "geralAdministrador.html";
     } else {
         alert("E-mail ou senha incorretos. Tente novamente.");
