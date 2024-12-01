f0937cecac11ece9cb1289e88ae2626913c5af9f
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-12-01 14:44:30 -0300

    feat: request to news and animals

--- js/fetch.js
+++ js/fetch.js
@@ -15,9 +15,39 @@ const projectsMain = async () => {
     }
 }
 
+const animalsMain = async () => {
+    try {
+        const response = await fetch(`${apiUrl}projetos`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json()
+        return data // Retorna os dados em JSON
+    } catch (error) {
+        console.error('Houve um problema com a requisição Fetch:', error);
+    }
+}
+
+const newsMain = async () => {
+    try {
+        const response = await fetch(`${apiUrl}noticias`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json()
+        return data // Retorna os dados em JSON
+    } catch (error) {
+        console.error('Houve um problema com a requisição Fetch:', error);
+    }
+}
+
 const onMain = async () => {
-    let project = await projectsMain();
+    let project = await projectsMain()
     console.table(project); // Mostra os dados
+    let animals = await animalsMain()
+    console.table(animals)
+    let news = await newsMain()
+    console.table(news)
 }
 
 onMain();
