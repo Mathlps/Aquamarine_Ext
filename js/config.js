e9c90c696075cbcd55d19a8fd5fea39916f05543
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-12-05 20:25:15 -0300

    fix: animation problem fixed

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 const config = {
-    // API_URL: 'https://aquamarine-ext.onrender.com/', //api hosted
+    // API_URL: 'https://aquamarine-ext.onrender.com/', //api hosted 1
+    // API_URL: 'https://aquamarine-backend.vercel.app/', //api hosted 2
     API_URL: 'http://127.0.0.1:5000/', // local
 };
 
