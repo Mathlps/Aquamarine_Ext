fc3590be3a20d615d8bb49dc35259b60055cbb78
PaulingCavalcante <ph.doamaralcavalcante@gmail.com>
2024-12-11 18:01:01 -0300

    feat: login

--- Client/Pages/Adm/loginAdm.js
+++ Client/Pages/Adm/loginAdm.js
@@ -1,55 +1,55 @@
-const contaPermitida = {
-    email: "admin@aquamarine.com.br",
-    senha: "12345"
-};
-
-const loginForm = document.getElementById("loginForm");
-const emailInput = document.getElementById("email");
-const senhaInput = document.getElementById("senha");
-
-loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const email = emailInput.value;
-    const senha = senhaInput.value;
-
-    if (email === contaPermitida.email && senha === contaPermitida.senha) {
-        alert("Login realizado com sucesso!");
-        window.location.href = "geralAdministrador.html";
-    } else {
-        alert("E-mail ou senha incorretos. Tente novamente.");
-    }
-});
-
-// import config from 'config'; // Importa o config.js
+// const contaPermitida = {
+//     email: "admin@aquamarine.com.br",
+//     senha: "12345"
+// };
 
 // const loginForm = document.getElementById("loginForm");
 // const emailInput = document.getElementById("email");
 // const senhaInput = document.getElementById("senha");
 
-// loginForm.addEventListener("submit", async (event) => {
+// loginForm.addEventListener("submit", (event) => {
 //     event.preventDefault();
 
 //     const email = emailInput.value;
 //     const senha = senhaInput.value;
 
-//     alert(email + "\n" + senha)
-
-//     // Enviar requisição para o backend usando a API_URL do config
-//     const response = await fetch(`${config.API_URL}login`, { // Usa a API_URL
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ email, senha })
-//     });
-
-//     const data = await response.json();
-
-//     if (response.ok) {
+//     if (email === contaPermitida.email && senha === contaPermitida.senha) {
 //         alert("Login realizado com sucesso!");
-//         window.location.href = "geralAdministrador.html"; // Redireciona para a página do administrador
+//         window.location.href = "geralAdministrador.html";
 //     } else {
-//         alert(data.error || "E-mail ou senha incorretos. Tente novamente.");
+//         alert("E-mail ou senha incorretos. Tente novamente.");
 //     }
 // });
+
+import config from './config.js'; // Importa o config.js
+
+const loginForm = document.getElementById("loginForm");
+const emailInput = document.getElementById("email");
+const senhaInput = document.getElementById("senha");
+
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = emailInput.value;
+    const senha = senhaInput.value;
+
+    alert(email + "\n" + senha)
+
+    // Enviar requisição para o backend usando a API_URL do config
+    const response = await fetch(`${config.API_URL}login`, { // Usa a API_URL
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, senha })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        alert("Login realizado com sucesso!");
+        window.location.href = "geralAdministrador.html"; // Redireciona para a página do administrador
+    } else {
+        alert(data.error || "E-mail ou senha incorretos. Tente novamente.");
+    }
+});
